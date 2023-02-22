20fa0f462534b46c623fbdd9c3080ce305978036
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2023-02-22 08:28:19 +0100

    Hack: Allow broken links

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/hyper-db',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful

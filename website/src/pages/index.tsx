03e0a978f2fe7b14b81bfca54bed8d64c2b8de50
André Kohn <andre.kohn@salesforce.com>
2023-02-22 10:05:22 +0100

    Fix linter warnings

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -9,12 +9,12 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig, baseUrl} = useDocusaurusContext();
+  const {siteConfig} = useDocusaurusContext();
   const logoSrc = useBaseUrl('/img/hyper-logo.svg');
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <img src={logoSrc} className="hero__logo" className={styles.heroLogo} />
+        <img src={logoSrc} className={styles.heroLogo} />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>

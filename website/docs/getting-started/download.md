df24c95b7bfc4abb559efb5f7f8725157e25633d
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2023-02-24 15:18:45 +0100

    Add (overly simplistic) download page

---
+++ website/docs/getting-started/download.md
@@ -0,0 +1,25 @@
+---
+title: Download Hyper
+---
+
+import {siteConfig} from '@site/src/config';
+
+Hyper comes neatly packaged inside "Hyper API". Hyper API bundles the Hyper database together with a client-side library which offers utility functions for connection management, SQL formatting etc.
+
+You can download the latest version of Hyper API for your operating system from the following links:
+
+* Windows
+   * <a href={siteConfig.download.windows_py}>Python .whl</a>
+   * <a href={siteConfig.download.windows_cxx}>C++</a>
+   * <a href={siteConfig.download.windows_java}>Java</a>
+   * <a href={siteConfig.download.windows_dotnet}>.Net</a>
+* Mac
+   * <a href={siteConfig.download.macos_py}>Python .whl</a>
+   * <a href={siteConfig.download.macos_cxx}>C++</a>
+   * <a href={siteConfig.download.macos_java}>Java</a>
+   * <a href={siteConfig.download.macos_dotnet}>.Net</a>
+* Linux
+   * <a href={siteConfig.download.linux_py}>Python .whl</a>
+   * <a href={siteConfig.download.linux_cxx}>C++</a>
+   * <a href={siteConfig.download.linux_java}>Java</a>
+   * <a href={siteConfig.download.linux_dotnet}>.Net</a>
---
+++ website/src/config.ts
@@ -0,0 +1,23 @@
+const version = '0.0.16491.r416a9c30'
+const pyversion = version.substr(0, version.lastIndexOf("."));
+
+const downloadBaseUrl = 'https://downloads.tableau.com/tssoftware/'
+
+export const siteConfig = {
+   download: {
+      windows_py: `${downloadBaseUrl}/tableauhyperapi-${pyversion}-py3-none-win_amd64.whl`,
+      windows_cxx: `${downloadBaseUrl}/tableauhyperapi-cxx-windows-x86_64-release-main.${version}.zip`,
+      windows_java: `${downloadBaseUrl}/tableauhyperapi-java-windows-x86_64-release-main.${version}.zip`,
+      windows_dotnet: `${downloadBaseUrl}/tableauhyperapi-dotnet-windows-x86_64-main.${version}.zip`,
+
+      macos_py: `${downloadBaseUrl}/tableauhyperapi-${pyversion}-py3-none-macosx_10_11_x86_64.whl`,
+      macos_cxx: `${downloadBaseUrl}/tableauhyperapi-cxx-macos-x86_64-release-main.${version}.zip`,
+      macos_java: `${downloadBaseUrl}/tableauhyperapi-java-macos-x86_64-release-main.${version}.zip`,
+      macos_dotnet: `${downloadBaseUrl}/tableauhyperapi-dotnet-macos-x86_64-main.${version}.zip`,
+
+      linux_py: `${downloadBaseUrl}/tableauhyperapi-${pyversion}-none-manylinux2014_x86_64.whl`,
+      linux_cxx: `${downloadBaseUrl}/tableauhyperapi-cxx-linux-x86_64-release-main.${version}.zip`,
+      linux_java: `${downloadBaseUrl}/tableauhyperapi-java-linux-x86_64-release-main.${version}.zip`,
+      linux_dotnet: `${downloadBaseUrl}/tableauhyperapi-dotnet-linux-x86_64-main.${version}.zip`,
+   }
+}

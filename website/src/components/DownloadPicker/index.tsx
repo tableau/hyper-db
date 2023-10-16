79f4a19616d8279692f74745a4a82419548ab41b
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2023-10-16 09:47:50 +0200

    Remove the documentation for the .Net version of Hyper API (#54)

--- website/src/components/DownloadPicker/index.tsx
+++ website/src/components/DownloadPicker/index.tsx
@@ -1,41 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { config } from '@site/src/config';
 import { detectOS } from '@site/src/os_detection';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
-import Admonition from '@theme/Admonition';
 import styles from './styles.module.css';
 import LinuxIcon from '@site/static/img/devicon-linux.svg';
 import WindowsIcon from '@site/static/img/devicon-windows.svg';
 import MacosIcon from '@site/static/img/devicon-macos.svg';
 
-type DotNetWarningProps = {
-    url: string;
-};
-
-function DotNetWarning({ url }: DotNetWarningProps) {
-    return (
-        <div style={{ 'margin-top': '1em' }}>
-            <Admonition type="danger" title="Deprecation Warning">
-                The .NET version of Hyper API is deprecated. Older versions of Hyper API will still be available via
-                NuGET. In the future, .Net will not receive any new features or updates. The old packages will stay
-                available on NuGET indefinitely, so you can keep using older versions in .Net. The other languages
-                (Python, Java, C++) are not impacted by this in any way. In case this is causing issues for you, please
-                reach out via{' '}
-                <a href="https://join.slack.com/t/tableau-datadev/shared_invite/zt-1q4rrimsh-lHHKzrhid1MR4aMOkrnAFQ">
-                    Slack
-                </a>
-                .
-                <br />
-                <br />
-                <a href={url}>Download the .Net Hyper API</a>
-            </Admonition>
-        </div>
-    );
-}
-
 export function DownloadPicker() {
-    const [displayDotnet, setDisplayDotnet] = useState(false);
     return (
         <Tabs defaultValue={detectOS()}>
             <TabItem
@@ -55,12 +28,6 @@ export function DownloadPicker() {
                     </li>
                     <li>
                         <a href={config.download.windows_java}>Java (Windows)</a>
-                    </li>
-                    <li>
-                        <a href="#" onClick={() => setDisplayDotnet(true)}>
-                            .Net (Windows)
-                        </a>
-                        {displayDotnet ? <DotNetWarning url={config.download.windows_dotnet} /> : <></>}
                     </li>
                 </ul>
             </TabItem>
@@ -82,12 +49,6 @@ export function DownloadPicker() {
                     <li>
                         <a href={config.download.macos_java}>Java (macOS)</a>
                     </li>
-                    <li>
-                        <a href="#" onClick={() => setDisplayDotnet(true)}>
-                            .Net (macOS)
-                        </a>
-                        {displayDotnet ? <DotNetWarning url={config.download.macos_dotnet} /> : <></>}
-                    </li>
                 </ul>
             </TabItem>
             <TabItem
@@ -107,12 +68,6 @@ export function DownloadPicker() {
                     </li>
                     <li>
                         <a href={config.download.linux_java}>Java (Linux)</a>
-                    </li>
-                    <li>
-                        <a href="#" onClick={() => setDisplayDotnet(true)}>
-                            .Net (Linux)
-                        </a>
-                        {displayDotnet ? <DotNetWarning url={config.download.linux_dotnet} /> : <></>}
                     </li>
                 </ul>
             </TabItem>

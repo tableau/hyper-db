tableau/hyper-db

commit 1960cc6da7f29d6aa9053a1de4d86d71dd384444

Hyper API June 2024 Release (#99)
* Add upcoming section
* Document file format 3 (#93)
* Add release notes for upcoming 32-bit floating points (#92)
* Document null handling of || (#95)
* Document `regexp_replace` (#96)
* Prepare the June 2024 release
* Update release notes
* Update release version
* Add Tableau versions that will support file format 4.
* Apple Silicon updates (#101)

---------

Co-authored-by: wolfroediger <109283644+wolfroediger@users.noreply.github.com>
Co-authored-by: Jan Böttcher <6001032+jboettcher@users.noreply.github.com>
Co-authored-by: Florian Scheibner <fscheibner@salesforce.com>
Co-authored-by: Dimitri Vorona <dvorona@tableau.com>
Co-authored-by: Justine D'Souza <justine.dsouza@salesforce.com>

website/src/config.ts
@@ -1,4 +1,4 @@
-const version_long = '0.0.18825.rd90b6d69';
+const version_long = '0.0.19484.rdb1797f2';
 const version_short = version_long.substr(0, version_long.lastIndexOf('.'));
 
 const downloadBaseUrl = 'https://downloads.tableau.com/tssoftware/';
@@ -13,7 +13,11 @@ export const config = {
         macos_cxx: `${downloadBaseUrl}/tableauhyperapi-cxx-macos-x86_64-release-main.${version_long}.zip`,
         macos_java: `${downloadBaseUrl}/tableauhyperapi-java-macos-x86_64-release-main.${version_long}.zip`,
 
-        linux_py: `${downloadBaseUrl}/tableauhyperapi-${version_short}-py3-none-manylinux2014_x86_64.whl`,
+        macos_arm_py: `${downloadBaseUrl}/tableauhyperapi-${version_short}-py3-none-macosx_13_0_arm64.whl`,
+        macos_arm_cxx: `${downloadBaseUrl}/tableauhyperapi-cxx-macos-arm64-release-main.${version_long}.zip`,
+        macos_arm_java: `${downloadBaseUrl}/tableauhyperapi-java-macos-arm64-release-main.${version_long}.zip`,
+
+        linux_py: `${downloadBaseUrl}/tableauhyperapi-${version_short}-py3-none-macosx_13_0_arm64.whl`,
         linux_cxx: `${downloadBaseUrl}/tableauhyperapi-cxx-linux-x86_64-release-main.${version_long}.zip`,
         linux_java: `${downloadBaseUrl}/tableauhyperapi-java-linux-x86_64-release-main.${version_long}.zip`,
 

website/src/components/AppleSiliconWorkarounds.tsx
@@ -1,35 +1,0 @@
-import React from 'react';
-import Admonition from '@theme/Admonition';
-
-export function AppleSiliconWorkarounds() {
-    return (
-        <Admonition type="info" title="Apple Silicon Support">
-            If you are on Apple Silicon (Apple M1, Apple M2, ...), you will currently need workarounds, as described
-            below.
-            <details>
-                <summary style={{ fontWeight: 'bold', margin: '.8em .5em' }}>Apple Silicon Workarounds</summary>
-                <div style={{ marginLeft: '1.4em' }}>
-                    <p>
-                        Hyper API runs on Apple Silicon only with{' '}
-                        <a href="https://support.apple.com/en-us/HT211861">Rosetta 2</a> instead of natively. You will
-                        have to install Rosetta 2 before installing Hyper API.
-                    </p>
-                    <p>
-                        Furthermore, this means that Hyper API can only be used from within other x86 programs, running
-                        under Rosetta. In particular for Python and Java, this means that the Python interpreter/Java
-                        runtime needs to run with Rosetta, too.
-                    </p>
-                    <p>
-                        To do so, either install the Intel-only version of the Python/Java interpreter, or prefix your
-                        call with <code>arch -x86_64</code>. E.g., for Python, you can use:
-                    </p>
-                    <pre>
-                        arch -x86_64 /usr/bin/python3 -m pip install tableauhyperapi
-                        <br />
-                        arch -x86_64 /usr/bin/python3 your_script.py
-                    </pre>
-                </div>
-            </details>
-        </Admonition>
-    );
-}

website/src/components/DownloadPicker/index.tsx
@@ -41,13 +41,22 @@ export function DownloadPicker() {
             >
                 <ul>
                     <li>
-                        <a href={config.download.macos_py}>Python .whl (macOS)</a>
+                        <a href={config.download.macos_py}>Python .whl (macOS x64)</a>
                     </li>
                     <li>
-                        <a href={config.download.macos_cxx}>C++ (macOS)</a>
+                        <a href={config.download.macos_arm_py}>Python .whl (macOS ARM)</a>
                     </li>
                     <li>
-                        <a href={config.download.macos_java}>Java (macOS)</a>
+                        <a href={config.download.macos_cxx}>C++ (macOS x64)</a>
+                    </li>
+                    <li>
+                        <a href={config.download.macos_arm_cxx}>C++ (macOS ARM)</a>
+                    </li>
+                    <li>
+                        <a href={config.download.macos_java}>Java (macOS x64)</a>
+                    </li>
+                    <li>
+                        <a href={config.download.macos_arm_java}>Java (macOS ARM)</a>
                     </li>
                 </ul>
             </TabItem>

commit 3e73609cea6260f5b16596e4c0aa094be5ee5380

Fix linux download link
The previous commit 1960cc6 mistakenly changed the Linux download link to be the same as the MacOS one. This fixes it.

website/src/config.ts
@@ -17,7 +17,7 @@ export const config = {
         macos_arm_cxx: `${downloadBaseUrl}/tableauhyperapi-cxx-macos-arm64-release-main.${version_long}.zip`,
         macos_arm_java: `${downloadBaseUrl}/tableauhyperapi-java-macos-arm64-release-main.${version_long}.zip`,
 
-        linux_py: `${downloadBaseUrl}/tableauhyperapi-${version_short}-py3-none-macosx_13_0_arm64.whl`,
+        linux_py: `${downloadBaseUrl}/tableauhyperapi-${version_short}-py3-none-manylinux2014_x86_64.whl`,
         linux_cxx: `${downloadBaseUrl}/tableauhyperapi-cxx-linux-x86_64-release-main.${version_long}.zip`,
         linux_java: `${downloadBaseUrl}/tableauhyperapi-java-linux-x86_64-release-main.${version_long}.zip`,
 